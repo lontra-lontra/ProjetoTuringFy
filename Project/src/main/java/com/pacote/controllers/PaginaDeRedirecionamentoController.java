3bd8636e3f59c8635c75c2f1c1aa68bbc846d43b
Gustavo Scardino <gustavoscardino@usp.br>
2022-05-18 22:42:53 -0300

    Commit da Entrega 1

---
+++ Project/src/main/java/com/pacote/controllers/PaginaDeRedirecionamentoController.java
@@ -0,0 +1,21 @@
+package com.pacote.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+//import se.michaelthelin.spotify.model_objects.specification.Track;
+
+@Controller
+public class PaginaDeRedirecionamentoController {
+
+	@GetMapping("/red")
+	public String greeting(@RequestParam(name="code", required=false, defaultValue="") String code, Model model) {
+		//System.out.println(code);
+		ComunicadorDoSpotify.setCodigoDeAutorização(code);
+		ComunicadorDoSpotify.pegaAutorizaçãoDeTabela();
+		return "paginaDeRedirecionamentoView";	
+	}
+
+}
