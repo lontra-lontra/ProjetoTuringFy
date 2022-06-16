e29dc97bdbcc32ee22915d556cca4573babf97b1
JuliaCMuniz <juliamuniz.cm@usp.br>
2022-06-15 21:24:09 -0300

    Alterando Playlists Internas Spotify

---
+++ Project/src/main/java/com/pacote/controllers/EditorDePlaylists.java
@@ -0,0 +1,68 @@
+package com.pacote.controllers;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hc.core5.http.ParseException;
+
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.credentials.ClientCredentials;
+import se.michaelthelin.spotify.model_objects.specification.Playlist;
+import se.michaelthelin.spotify.requests.data.playlists.ChangePlaylistsDetailsRequest;
+import se.michaelthelin.spotify.requests.data.playlists.CreatePlaylistRequest;
+import se.michaelthelin.spotify.requests.data.playlists.GetListOfCurrentUsersPlaylistsRequest;
+import se.michaelthelin.spotify.requests.data.follow.legacy.UnfollowPlaylistRequest;
+
+
+
+public class EditorDePlaylists {
+	
+	public List<Playlist> getUsersPlaylists(){
+		GetItemFromSimplifiedType conversor = new GetItemFromSimplifiedType();
+		List<Playlist> playlistsDoUsuário = new ArrayList<>();
+		
+		GetListOfCurrentUsersPlaylistsRequest getListOfCurrentUsersPlaylistsRequest = ComunicadorDoSpotify.spotifyApi
+			    .getListOfCurrentUsersPlaylists().build();
+		try {
+			playlistsDoUsuário = conversor.getFromSimplified(getListOfCurrentUsersPlaylistsRequest.execute().getItems());
+		} catch (ParseException | SpotifyWebApiException | IOException e) {
+			System.out.println("Playlist não encontrada: " + e);
+		}
+		return playlistsDoUsuário;
+		
+	}
+	
+	public void deleteUsersPlaylist(String playlistID) {
+		try {
+		    final ClientCredentials nossasCredenciais = ComunicadorDoSpotify.RequestDasNossasCredenciais.execute();
+		    UnfollowPlaylistRequest.Builder construtor = new UnfollowPlaylistRequest.Builder(nossasCredenciais.getAccessToken());
+		    construtor.owner_id(ComunicadorDoSpotify.spotifyApi.getClientId());
+		    construtor.playlist_id(playlistID);
+		    UnfollowPlaylistRequest removePlaylist = construtor.build();
+		    removePlaylist.execute();		    
+		  } catch (IOException | SpotifyWebApiException | ParseException e) {
+		   System.out.println("Error: " + e.getMessage());
+		  }		
+	}
+	
+	public void createUsersPlaylist(String playlistName) {
+		CreatePlaylistRequest createPlaylistRequest = ComunicadorDoSpotify.spotifyApi.createPlaylist(ComunicadorDoSpotify.spotifyApi.getClientId(), playlistName).build();
+		 try {
+		      createPlaylistRequest.execute();
+		    } catch (IOException | SpotifyWebApiException | ParseException e) {
+		      System.out.println("Error: " + e.getMessage());
+		    }	
+	}
+	
+	public void changePlaylistName(String newName, String playlistID) {
+		ChangePlaylistsDetailsRequest changePlaylistsDetailsRequest = ComunicadorDoSpotify.spotifyApi
+			    .changePlaylistsDetails(playlistID).name(newName).build();
+		try {
+		      changePlaylistsDetailsRequest.execute();
+		    } catch (IOException | SpotifyWebApiException | ParseException e) {
+		      System.out.println("Error: " + e.getMessage());
+		    }
+		
+	}
+
+}
