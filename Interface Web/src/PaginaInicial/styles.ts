0c8af359a58ba1bccc9cf96c31e33a3efabaa61e
Juliana Marcarini <julianamarcarinic@gmail.com>
2022-06-18 20:10:42 -0300

    Altera design da interface web e cria protótipo da página Resultados

--- Interface Web/src/PaginaInicial/styles.ts
+++ Interface Web/src/PaginaInicial/styles.ts
@@ -10,14 +10,14 @@ export const CorpoTodo = styled.div`
 `
 export const header = styled.header`
     width: 100%;
-    height: 160px;
+    height: 17vh;
     background-color: black;
     display: flex;
     flex-direction:row ;
     justify-content:baseline;
 `
 
-export const corpo = styled.body`
+export const corpo = styled.div`
     display: flex;
     flex-direction: row;
 `
@@ -46,11 +46,12 @@ export const Slogan = styled.p`
 export const ColunaEsquerda = styled.div`
     align-items: center;
     width: 20%;
-    height:800px;
+    height:fit-content;
     background-color: #0F0F0F;
     display: flex;
     flex-direction: column;
-    justify-content:space-around; 
+    justify-content:space-around;
+    min-height:83vh;
 `
 
 export const SuasPlaylists = styled.div`
@@ -75,21 +76,64 @@ export const SuasPlaylistsNomes = styled.button`
 `
 
 export const BlocoDeBusca = styled.div`
+    display: flex;
+    flex-direction: column;
     width: 80%;
     background-color: #1B1B1B;
     color: white;
     font-family: 'Nunito Sans', sans-serif;
+    align-items: flex-start;
 `
 
 export const Buscar = styled.h2`
-    margin-top: 80px;
     font-size: 56px;
-    margin-left: 64px;
     font-family: 'Nunito Sans', sans-serif;
+    margin-left: 48px;
+    height: min-content;
+    margin-bottom: 8px;
 `
 
 export const DescricaoBusca = styled.p`
-    margin-left: 64px;
+    margin-top: 0px;
+    margin-left: 48px;
     font-size: 24px;
     font-family: 'Nunito Sans', sans-serif;     
+`
+export const BarraBuscaEFiltro = styled.div`
+    display: flex;
+    margin-left: 48px;
+    gap:16px;
+`
+export const BotaoFiltro = styled.button`
+    background-color: #1B1B1B;
+    cursor: pointer;
+    border-color: #1B1B1B;
+`
+export const BarraBusca = styled.input`
+
+    width: 943px;
+    height: 67px;
+    background: #0F0F0F;
+    border-radius: 37px;
+    font-size: 32px;
+    font-family: 'Nunito Sans', sans-serif;    
+    color: white;
+    padding-left: 24px;
+`
+
+export const BotaoBuscar = styled.button`
+    display: flex;
+    width: 160px;
+    height: 48px;
+    background-color:#0CC629;
+    opacity: 0.7;
+    color:white;
+    font-size: 40px;    
+    font-family: 'Nunito Sans', sans-serif;    
+    border-radius:15px;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    margin-top: 16px;
+    margin-left: 48px;
 `
